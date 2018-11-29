031fcde9ef6e1e527e514f852db500400bffedf6
David Heremans <dhran@scarlet.be>
2018-11-29 10:20:23 +0100

    big scroller flawlessly

--- test.tcl
+++ test.tcl
@@ -1,14 +1,19 @@
+# scrollpatterncombinedb is cc0d
+# zzzz after the call cleanupscrollpatterncombinedb is 5f6b
 proc show_combiners {} {
 	variable x
 	lappend sh
-	set x [expr {0xCA4D}]
+	set x [expr {0xCC0D}]
 	while { [peek $x] != 255 } {
 			set a [peek $x]
 			set b [peek [expr {$x + 1}]]
-			puts "$a + $b"
-			lappend sh "$a + $b"
+			set c [peek_s16 [expr {$x + 2}]]
+			lappend sh "$a+$b:$c   "
 			set x [expr {$x + 20}]
 	}
 	puts [lsort $sh]
 	puts [llength $sh]
+	puts " "
 }
+
+debug set_bp 0x5F6B {1 == 1} {show_combiners}
